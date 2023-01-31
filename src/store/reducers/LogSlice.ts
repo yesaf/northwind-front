ef555c200a19d138850675c1447c66e4cddbaaf4
Yevhenii Yesafiev <yesaf.eugene@gmail.com>
2023-01-31 12:30:14 +0200

    fixed: dashboard keywords count

--- src/store/reducers/LogSlice.ts
+++ src/store/reducers/LogSlice.ts
@@ -39,9 +39,9 @@ export const logSlice = createSlice({
         addLog: (state, action: PayloadAction<ILog>) => {
             state.log = [action.payload, ...state.log];
             state.queryCount++;
-            state.keywordsCount.select += action.payload.query.split('SELECT').length - 1;
-            state.keywordsCount.where += action.payload.query.split('WHERE').length - 1;
-            state.keywordsCount.leftJoin += action.payload.query.split('JOIN').length - 1;
+            state.keywordsCount.select += action.payload.query.split(/select/i).length - 1;
+            state.keywordsCount.where += action.payload.query.split(/where/i).length - 1;
+            state.keywordsCount.leftJoin += action.payload.query.split(/join/i).length - 1;
         },
         addResultCount: (state, action: PayloadAction<number>) => {
             state.resultCount += action.payload;
